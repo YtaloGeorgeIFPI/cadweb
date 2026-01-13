c8124abe3e7e9cf340844d43d84c9858cbfb25e8
Ytalo George <ytalogsilva@gmail.com>
2026-01-12 21:08:40 -0300

    CRUD

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,11 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('categoria/', views.categoria, name="categoria"),
     path('categoria/form', views.form_categoria, name="form_categoria"),
+    path('categoria/editar/<int:id>/', views.editar_categoria, name="editar_categoria"),
+    
+    # URL para detalhes da categoria
+    path('categoria/detalhes/<int:id>/', views.detalhes_categoria, name="detalhes_categoria"),
+    
+    # URL para excluir a categoria
+    path('categoria/remover/<int:id>/', views.remover_categoria, name="remover_categoria"),
 ]
-
--- home/views.py
+++ home/views.py
@@ -1,30 +1,62 @@
 from django.shortcuts import render, redirect
-
-from .models import *
-
-from .forms import *
-
+from django.contrib import messages
+from .models import Categoria
+from .forms import CategoriaForm
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 def categoria(request):
     contexto = {
         'lista': Categoria.objects.all().order_by('-id'),
     }
-    return render(request, 'categoria/lista.html',contexto)
+    return render(request, 'categoria/lista.html', contexto)
 
 def form_categoria(request):
     if request.method == 'POST':
-       form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
-       if form.is_valid():# faz a validação do formulário
-            form.save() # salva a instancia do modelo no banco de dados
-            return redirect('categoria') # redireciona para a listagem
-    else:# método é get, novo registro
-        form = CategoriaForm() # formulário vazio
+        form = CategoriaForm(request.POST)  # Instancia o modelo com os dados do form
+        if form.is_valid():  # Faz a validação do formulário
+            form.save()  # Salva a instância do modelo no banco de dados
+            messages.success(request, 'Operação realizada com sucesso!')  # Mensagem de sucesso
+            return redirect('categoria')  # Redireciona para a listagem
+    else:  # Método é GET, novo registro
+        form = CategoriaForm()  # Formulário vazio
     contexto = {
-        'form':form,
+        'form': form,
     }
     return render(request, 'categoria/formulario.html', contexto)
 
+def editar_categoria(request, id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('categoria')  # Redireciona para a listagem
+    
+    if request.method == 'POST':
+        # Combina os dados do formulário com a instância do objeto existente, permitindo editar seus valores
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            categoria = form.save()  # Save retorna o objeto salvo
+            messages.success(request, 'Operação realizada com Sucesso')  # Mensagem de sucesso
+            return redirect('categoria')  # Redireciona para a listagem
+    else:
+        form = CategoriaForm(instance=categoria)
+    return render(request, 'categoria/formulario.html', {'form': form,})
 
+# Função para mostrar os detalhes de uma categoria
+def detalhes_categoria(request, id):
+    categoria = Categoria.objects.get(pk=id)
+    return render(request, 'categoria/detalhes.html', {'categoria': categoria})
+
+# Função para remover uma categoria
+def remover_categoria(request, id):
+    try:
+        categoria = Categoria.objects.get(pk=id)  # Tentando encontrar a categoria
+        categoria.delete()  # Exclui a categoria
+        messages.success(request, 'Categoria removida com sucesso.')  # Mensagem de sucesso
+    except Categoria.DoesNotExist:
+        messages.error(request, 'Categoria não encontrada.')  # Mensagem de erro se a categoria não existir
+
+    return redirect('categoria')  # Redireciona para a listagem de categorias
